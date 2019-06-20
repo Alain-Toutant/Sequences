71e2c03cc58e878984e22eaf34b62718d6eb4be3
Alain-Toutant <51757557+Alain-Toutant@users.noreply.github.com>
2019-06-19 21:00:03 -0400

    Add files via upload

--- fibonacci.py
+++ fibonacci.py
@@ -4,6 +4,72 @@ def fibo(N):
     for _ in range(1,N): a,b = b,a+b
     return a,b
 
+# find starting values (greater than zero)
+# from two sequenctial values and factors
+from math import gcd
+def findBase(f0,f1,a,b):
+    c = f0*a+f1*b
+    while a>0:
+        a,b,c = (b-f1*a)//f0,a,b
+    return b,c
+
+"""
+   a*f0 + b*f1
+   (a+b)*f0 + b*f1-b*f0
+   (a+b)*f0 + b*(f1-f0) = c
+"""
+# Determines factors and base from 4 consecutive numbers of the sequence
+# (math)
+#
+# returns A,B,f(1),f(2)  where: f(n) = A*f(n-2) + B*f(n+1)
+#
+# e.g.    isfibo(2, 8, 34, 144) --> (1, 4, 2, 8)T
+#
+#            meaning: f(n)= 1*f(n-2) + 4*f(n-1),  f(1)=2, f(2)=8
+#
+#            Sequence(1,4,2,8) if the sequence of even fibonacci numbers
+#
+def isFibo(a,b,c,d):
+    n  = (a*d - b*c)
+    m  = (a*c - b**2)
+    if m == 0 or n%m : return None
+    f1 = n//m
+    n,m  = (c-f1*b),a
+    if m == 0: n,m = (d-f1*c),b    
+    if n%a: return None
+    f0 = n//a
+    a,b = findBase(f0,f1,a,b)
+    return f0,f1,a,b
+
+# find the factors and base from 3 consecutive terms
+# (iterative)
+#
+def findFibo(a,b,c):
+    m = (c % a)
+    for k in range(m,c+1,b):
+        f1 = (k-m)//b
+        f0 = (c - f1*b)//a
+        if f0<=0: break
+        if a*f0+b*f1 != c: continue
+        a,b = findBase(f0,f1,a,b)
+        return f0,f1,a,b
+    
+
+        
+# infinite iterator of fibonacci numbers
+#
+#  starting from zero:  0,1,1,2,3,5,8,...
+#
+# example use: sum of first 10 even fibonacci:
+#
+#   sum(islice(iFibo(),3,10*3,3)) == 257114
+#
+def iFibo(): 
+    a,b = 0,1
+    while True:
+        yield a
+        a,b = b,a+b
+
 # List of N fibonacci numbers
 def listFibo(N):
     result = [1,1][:N]
@@ -56,7 +122,7 @@ def fastFibo(N):
 
 # sum of first N fibonacci numbers (exponential recursive) O(Log(N)^2) time
 def fastSumFibo(N,rec=False):
-    if N < 1: return (0,1) if rec else 0
+    if N < 2: return [(0,1),(1,1)][N] if rec else [0,1][N]
     m     = N//2
     f0,f1 = fastFibo(m-1)
     a,b   = fastSumFibo(m,True)
@@ -74,9 +140,9 @@ def fastEvenFibo(N):
 # sum of the first N even fibinacci numbers O(log(N)^2) time
 
 def fastSumEvenFibo(N,rec=False):
-    if N < 1: return (0,2) if rec else 0
+    if N < 2: return [(0,2),(2,8)][N] if rec else [0,2][N]
     m     = N//2
-    f0,f1 = fastEvenFibo(m-1)
+    f0,f1 = fastEvenFibo(m-1) 
     p0,p1 = f0//2,f1//2
     a,b   = fastSumEvenFibo(m,True)
     r     =  a + p0*a+p1*b
@@ -86,6 +152,11 @@ def fastSumEvenFibo(N,rec=False):
         r,s = r+d0, s+d1
     return (r,s) if rec else r
 
+"""
+[2, 8, 34]
+144, 610, 2584, 10946, 46368, 196418, 832040]
+
+"""
 
 
 # Sequence: Generic class for all fibonacci like sequences (of 2 terms)
@@ -139,7 +210,7 @@ def fastSumEvenFibo(N,rec=False):
 # f.sumRange(50,144) --> 288   Sum of values in the range
 #                              like: sum(f.inRange(50,144)) but faster
 # 
-
+from math import log
 class Sequence:
 
     def __init__(self,factor0=1,factor1=1,base0=1,base1=1):
@@ -147,13 +218,19 @@ class Sequence:
         self.f1 = factor1
         self.b0 = base0
         self.b1 = base1
+        self.factors = [(0,1),(factor0,factor1)] # powers of 2 (0,1,...)
 
-    def next(self,a, b, count=None):
+    def next(self,a=0, b=None, count=None):
         result = []
-        for _ in range(count or 1):
+        steps  = count or 1
+        if b is None:
+            a,b = self.b0,self.b1
+            result=[a,b]
+            steps -= 2
+        for _ in range(steps):
             a,b = b, self.f0*a + self.f1*b
             result.append(b)
-        return result if count else result[0]
+        return result[:count] if count else result[0]
 
     def previous(self,a,b,count=None):
         result = []
@@ -161,6 +238,24 @@ class Sequence:
             a,b = (b - self.f1*a)//self.f0,a
             result.insert(0,a)
         return result if count else result[0]
+
+    def __getitem__(self,index):
+        if isinstance(index,slice):
+            def generate():
+                step  = index.step or 1
+                m0,m1 = self.stepFactors(step)
+                a,b   = self.nth(index.start or 1,count=2)
+                n     = index.start or 1
+                while index.stop is None or n*step <= index.stop*step:                    
+                    yield a
+                    n += step
+                    if self.f0 == 1:
+                        a,b = m0*a+m1*b,m0*b+m1*self.next(a,b)
+                    else:
+                        a,b = self.nthFrom(a,b,step+1)
+                return
+            return generate()
+        return self.nth(index)
 
     def list(self,start,stop=None,step=None):
         if stop == None: start,stop = 1,start
@@ -186,69 +281,75 @@ class Sequence:
         value = self[c]
         return value if value < N else self[c-1]
 
-    def nth(self,N,count=1):        
-        a,b    = self.b0, self.b1  # value at 1
-        r,s    = (a,b) if N&1 else (0,a)
-        N    //= 2
-        a,b   = b,self.next(a,b)  # value at 2,
-        f0,f1 = self.f0, self.f1  # advance by 2
-        while N > 0:
-            if N&1:
-                if r : r,s = f0*r+f1*s, f0*s+f1*self.next(r,s)
-                else : r,s = a,b
-            a, b  = f0*a+f1*b,f0*b+f1*self.next(a,b) # value at 4, 8, 16, ...
-            f0,f1  = self.previous(a,b)//self.b0,a//self.b0
-            N //= 2
-        if count == 1: return r
-        if count == 2: return [r,s]
-        return [r,s]+self.next(r,s,count-2)
+    def binFactors(self,p):
+        if p < len(self.factors): return self.factors[p]
+        F0,B0  = self.f1,self.b0
+        while p >= len(self.factors):
+           f0,f1 = self.factors[-1]
+           a,b   = self.b0,self.b1
+           a,b   = f0*a+f1*b,f0*b+f1*self.next(a,b)
+           a,b,c = self.previous(a,b,2)+[a]
+           f0,f1 = (f0*a+f1*b)//B0, (f0*b+f1*c)//B0
+           self.factors.append((f0,f1))            
+        return f0,f1
 
+    def stepFactors(self,N):
+        r,s = 1,0
+        p   = -1
+        while N > 0:
+            pBit,N = N&1,N//2
+            p     += 1
+            if not pBit: continue
+            f0,f1 = self.binFactors(p)
+            if s==0: r,s = f0,f1
+            else:    r,s = f0*r+f1*s, f0*s+f1*self.next(r,s)
+        return r,s        
+
+    def nthFrom(self,a,b,N):
+        if N==0: return self.previous(a,b),a
+        if N==1: return a,b
+        if self.f0 != 1:
+            for _ in range(N-1):
+                a,b=b,self.next(a,b)
+            return a,b
+        c = self.next(a,b)
+        if N==2: return b,c
+        f0,f1 = self.stepFactors(N-1)        
+        return f0*a+f1*b, f0*b+f1*c
+    
+    def nth(self,N,count=None):
+        r,s = self.nthFrom(self.b0,self.b1,N)
+        if count is None: return r
+        if count < 3    : return [r,s][:count]
+        return [r,s]+self.next(r,s,count-2)
+        
     def count(self,start,stop=None):
         if stop is None: start,stop = 0,start
         def getCount(N):
-            lo,hi  = 0,N
-            result = 0
+            lo,hi  = 0,int(log(N+1,2))*2
             while hi >= lo:
                 mid = (hi+lo)//2
-                x = self[mid]
-                if x > N :
-                    hi = mid - 1
-                else:
-                    result = mid
-                    if x == N: break
-                    lo = mid + 1                 
-            return result
+                x   = self[mid]
+                if  x > N : hi = mid - 1
+                elif x < N: lo = mid + 1
+                else: return mid
+            return lo-1
         return getCount(stop)-getCount(start)
              
-    def __getitem__(self,index):
-        if isinstance(index,slice):
-            def generate():
-                step  = index.step or 1
-                m0,m1 = self.nth(step-1,count=2)
-                m0,m1 = m0//self.b0, m1//self.b0
-                a,b   = self.nth(index.start or 1,count=2)
-                n     = index.start or 1
-                while index.stop is None or n*step <= index.stop*step:
-                    n += step
-                    yield a
-                    a,b = m0*a+m1*b,m0*b+m1*self.next(a,b)
-                return
-            return generate()
-        return self.nth(index)
-
     def sum(self,start,stop=None):
         if stop is None: start,stop = 0,start
+        if self.f0 != 1: return sum(self[start:stop])
+        
         def accum(N):
             if N < 1 : return 0, self.b0
             if N == 1: return self.b0, self.b1
             m     = N//2
-            f0,f1 = self.nth(m-1,2)
-            f0,f1 = f0//self.b0, f1//self.b0
             a,b   = accum(m)
+            f0,f1 = self.stepFactors(m)
             r,s   = a+f0*a+f1*b, b+f0*b+f1*self.next(a,b)
             if N&1:
                 d0,d1 = self.nth(N,2)
-                r,s = r+d0, s+d1
+                r,s   = r+d0, s+d1
             return (r,s)
         return accum(stop)[0]-accum(start-1)[0]
 
@@ -262,6 +363,7 @@ class Sequence:
 if __name__ == "__main__":
     f = Sequence()
     f2 = Sequence(1,4,2,8) 
+    from itertools import accumulate,islice
 
     #############  TESTS ###################
        
@@ -331,11 +433,17 @@ if __name__ == "__main__":
     count = 1
     N = 40000
 
+    t= timeit(lambda:Sequence().sum(N), number=count) 
+    print(f"{count} x Sequence().sum({N})",t)
+
     t= timeit(lambda:fastSumFibo(N), number=count) 
     print(f"{count} x fastSumFibo({N})",t)
 
     t= timeit(lambda:sumFibo(N), number=count) 
     print(f"{count} x sumFibo({N})",t)
+
+    t= timeit(lambda:Sequence(1,4,2,8).sum(N), number=count) 
+    print(f"{count} x Sequence(1,4,2,8).sum({N})",t)
 
     t= timeit(lambda:fastSumEvenFibo(N), number=count) 
     print(f"{count} x fastSumEvenFibo({N})",t)
